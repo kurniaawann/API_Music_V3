d936eb53c29c049820c48b683eda8c933078a729
kurniawan <kurni.aawann19@gmail.com>
2024-11-08 14:20:52 +0700

    add schema min and max year

--- src/validator/song/scheme.js
+++ src/validator/song/scheme.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 const SongPayloadSchema = Joi.object({
   title: Joi.string().required(),
-  year: Joi.number().required(),
+  year: Joi.number().required().min(2022).max(2024),
   genre: Joi.string().required(),
   performer: Joi.string().required(),
   duration: Joi.number(),
