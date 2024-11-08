c08d5e059b4cc962ea4f530ef2b06deee31db0a7
kurniawan <kurni.aawann19@gmail.com>
2024-11-08 20:09:38 +0700

    add min and max yaer

--- src/validator/album/scheme.js
+++ src/validator/album/scheme.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 const AlbumPayloadSchema = Joi.object({
   name: Joi.string().required(),
-  year: Joi.number().required(),
+  year: Joi.number().required().min(2022).max(2024),
 });
 
 module.exports =  AlbumPayloadSchema ;
