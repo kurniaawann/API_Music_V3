66fd170c7101beb2dd348baea5e07fc8410b6d8d
kurniawan <kurni.aawann19@gmail.com>
2024-11-09 15:00:44 +0700

    add migration

---
+++ migrations/1731069592959_create-table-playlists.js
@@ -0,0 +1,23 @@
+
+exports.up = (pgm) => {
+    pgm.createTable('playlists', {
+      id: {
+        type: 'VARCHAR(50)',
+        primaryKey:true
+      },
+      name: {
+        type: 'TEXT',
+        notNull: true
+      },
+      owner: {
+        type: 'VARCHAR(50)',
+        references: '"users"', 
+        onDelete: 'CASCADE'
+      }
+
+    });
+  };
+   
+  exports.down = (pgm) => {
+    pgm.dropTable('playlists');
+  };
